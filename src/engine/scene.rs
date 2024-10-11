525548b6eff5624aedfac08ba3ee5ed2728daddb
Tiaan Louw <tiaanl@gmail.com>
2024-10-11 12:37:28 +0200

    Remove unused code.

--- src/engine/scene.rs
+++ src/engine/scene.rs
@@ -16,22 +16,3 @@ pub trait Scene {
 
     fn end_frame(&mut self) {}
 }
-
-pub struct SceneResource(pub Box<dyn Scene>);
-
-unsafe impl Sync for SceneResource {}
-unsafe impl Send for SceneResource {}
-
-impl std::ops::Deref for SceneResource {
-    type Target = dyn Scene;
-
-    fn deref(&self) -> &Self::Target {
-        self.0.as_ref()
-    }
-}
-
-impl std::ops::DerefMut for SceneResource {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        self.0.as_mut()
-    }
-}
