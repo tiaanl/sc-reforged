86f98df498bc1b84c98f0962d88ca0bfae263c17
Tiaan Louw <tiaanl@gmail.com>
2024-12-05 23:43:35 +0100

    Document the Scene functions.

--- src/engine/scene.rs
+++ src/engine/scene.rs
@@ -1,14 +1,23 @@
 use super::{input::InputState, renderer::Frame};
 
+/// A trait that represents a scene in the engine. I splits each stage of the render pipeline into
+/// separate function calls.
 #[allow(unused)]
 pub trait Scene {
+    /// Called when the size of the window surface is changed.
     fn resize(&mut self, width: u32, height: u32);
 
+    /// Called each frame with the `delta_time` based on the time the last frame took and the state
+    /// of all input devices.
     fn update(&mut self, delta_time: f32, input: &InputState);
 
-    fn debug_panel(&mut self, egui: &egui::Context) {}
-
+    /// Called before `render_frame`, but after `update` to allow render resources to be created and
+    /// updated before rendering.
     fn render_update(&mut self, device: &wgpu::Device, queue: &wgpu::Queue) {}
 
+    /// Called to render the the frame to the surface.
     fn render_frame(&self, frame: &mut Frame) {}
+
+    /// Called to allow debug panels to be added to the window.
+    fn debug_panel(&mut self, egui: &egui::Context) {}
 }
